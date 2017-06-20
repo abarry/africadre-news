3bc0cb656075dee93095890ef75fbcc01bf3cf0e
Montgomery MGOMRI <m.mgomri@prismamedia.com>
2017-06-21 00:22:06 +0200

    add crawlerbundle

---
+++ src/CrawlerBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace CrawlerBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('CrawlerBundle:Default:index.html.twig');
+    }
+}
